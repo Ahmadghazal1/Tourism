afdfe020f98133348c4069bf5df7b183fbc720a8
Ahmadghazal1 <74017377+Ahmadghazal1@users.noreply.github.com>
2023-01-13 04:47:13 +0300

    Toursim
    Web Design

---
+++ JavaSctipt/Home.js
@@ -0,0 +1,9 @@
+﻿function passvalues() {
+    var firstname = document.getElementById("form1Example13").value;
+ 
+    localStorage.setItem("textvalue", firstname);
+    return false;
+}
+
+document.getElementById("login").innerHTML = "Welcome " + localStorage.getItem("textvalue");
+
